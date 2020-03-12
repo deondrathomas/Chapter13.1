f064e0479d722c1ed3e065731f01f42f124009dd
deondrathomas <48733047+deondrathomas@users.noreply.github.com>
2020-03-12 13:22:05 -0500

    update looping medilib and image

--- src/MediaLib121/LoopingMediaLib.java
+++ src/MediaLib121/LoopingMediaLib.java
@@ -11,20 +11,23 @@ public class LoopingMediaLib {
 //            s = MediaFile.readString();
 //        }
 //        while (s != null) {
-//            String title = s.substring((s.indexOf("|") + 2));
-//            System.out.println("Title: " + title);
+//            String title = s.substring(0, s.indexOf("|"));
+//
+//
 //            String rating  = s.substring((s.indexOf("|") +1));
-//            System.out.println("Rating: " + rating);
+//            System.out.println("Song Title: " + title + "\nRating: " + rating);
 //            System.out.println();
 //            s = MediaFile.readString();
 //        }
         while (s != null) {
+            String title = s.substring(0, s.indexOf("|"));
             int rating = Integer.parseInt(s.substring((s.indexOf("|") +1)));
+
             if(rating >= 9) {
-                System.out.println("Rating: " + rating);
+                System.out.println("Song Title: " + title + "\nRating: " + rating);
                 s = MediaFile.readString();
                 Song song1 = new Song();
-                song1.setTitle("Mr. Clean");
+                song1.setTitle("Thats Tuff");
                 song1.setRating(10);
                 song1.setPrice(0.99);
 
